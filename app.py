46b6f5f5be0b49f16a03a229109fbfb0d49b4fa6
austin <business.mocksock@gmail.com>
2022-06-12 16:37:39 -0500

    basic notes for hello world app

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import  flask
+from flask import Flask
 
+# creates instance of the application
 app = Flask(__name__)
 
+# this is a decorator that shows the
+# url for this specific app
+
+# all routes need their own unique url
 @app.route('/')
+
+# Function that will be ran once 
+# the url is called
 def hello_world():
     return "Hello World!"
 
