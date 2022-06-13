08df6051c55213ec03af4aaf3b04ae43a59fe8f9
austin <business.mocksock@gmail.com>
2022-06-13 10:27:09 -0500

    notes added

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ app = Flask(__name__)
 # a string
 @app.route('/query')
 def get_string():
+    # this sets up the start of the request
     if request.args:
+        # defines variable so the arguments can be set up
         req=request.args
-        return " ".join(f"{k}: {v}" for k,v in req.items())
+        # so to get anything to happen, the url HAS to look like so:
+        # /query? then the information, such as John Doe
+        return " ".join(f"{k} {v}" for k,v in req.items())
 
+    # this is the return error 
     return "I made a Query but all I got was this lousy string"
 
 if __name__ == '__main__':
