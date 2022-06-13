dea17830501a707ccdd464eabcd5c8107f89b561
austin <business.mocksock@gmail.com>
2022-06-13 10:39:53 -0500

    begin basics of a collection

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # Let's learn about GET requests
+# First, GET needs a collection where 
+# it can "get" the data needed
+
+# let's use a pizza shop as an example
+
+# name of order
+order = {
+    # name of the first order
+    "order1": {
+
+    }
+
+}
 
 if __name__ == '__main__':
     # changes in code will show without 
