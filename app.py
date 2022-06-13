ef511b869ea7747547ca1c02ae6b4b514e64866c
austin <business.mocksock@gmail.com>
2022-06-13 10:17:12 -0500

    put all code down notes next

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
-# still need to import file
+from flask import Flask, request
 
 # creates instance of the application
 app = Flask(__name__)
 
 # Let's learn how to query
 # a string
+@app.route('/query')
+def get_string():
+    if request.args:
+        req=request.args
+        return " ".join(f"{k}: {v}" for k,v in req.items())
+
+    return "I made a Query but all I got was this lousy string"
 
 if __name__ == '__main__':
     # changes in code will show without 
